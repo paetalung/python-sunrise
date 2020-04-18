b2fdd7a86c08a197f464a646782d872bcac05562
plypun paetalung <32239930+paetalung@users.noreply.github.com>
2020-04-18 23:47:24 +0700

    1st commite
     sunrise time for VTSE

---
+++ sunrise.py
@@ -0,0 +1,46 @@
+# sunrise.py
+# https://paetalung.github.io/cv/
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from bs4 import BeautifulSoup as soup
+
+
+opt = webdriver.ChromeOptions()
+opt.add_argument('headless') # hidden mode of chrome
+
+driver = webdriver.Chrome(options=opt) # create driver
+
+url = 'https://meteogram.org/sun/thailand/chumphon/'
+
+driver.get(url) # open web
+time.sleep(3)   # waiting 3 sec
+
+page_html = driver.page_source
+driver.close()
+
+data = soup(page_html,'html.parser') # scan data
+#print(data)
+
+table = data.findAll('td',{'id':'sunrise'})
+#print(table)
+out = str(table) # index on data
+
+out = out.replace("[<td id=\"sunrise\">","")
+out = out.replace("</td>]","")
+out = out.replace("<span>","")
+out = out.replace("</span>","")
+out = out.replace("<strong>","")
+out = out.replace("</strong>","")
+#print(out)
+
+# Send line Section    
+from songline import Sendline
+token = '' # TEST TOKEN
+
+sunrise = "SUNRISE @ " + out
+
+messenger = Sendline(token)
+messenger.sendtext(sunrise)
+
